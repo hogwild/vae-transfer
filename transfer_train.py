8301bb3d15a3b686563adbec3e92ed35f5319490
hogwild <guxianbinmail@gmail.com>
2022-10-05 12:11:57 +0800

    'ver1'

--- transfer_train.py
+++ transfer_train.py
@@ -1,9 +1,9 @@
 from torch import optim
 from torchvision.utils import save_image
-import numpy as np
-import pickle
-import time
-import random
+# import numpy as np
+# import pickle
+# import time
+# import random
 import os
 from net_transfer import *
 from libs.Criterion import LossCriterion
@@ -13,32 +13,34 @@ from Matrix import MulLayer
 from net import VAE
 
 # from batch_provider import batch_provider
+os.makedirs('results_rec', exist_ok=True)
+os.makedirs('results_gen', exist_ok=True)
 
 torch.set_default_dtype(torch.float32)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model_path = 'VAEmodel_5layers.pkl'
+model_path = '/gpfsnyu/scratch/xg7/vae-transfer-data/VAEmodel_5layers.pkl'
 pretrained_model = torch.load(model_path, map_location=DEVICE)
-evaluator_path = 'VAEmodel_6layers.pkl'
+evaluator_path = '/gpfsnyu/scratch/xg7/vae-transfer-data/VAEmodel_6layers.pkl'
 pretrained_evaluator = torch.load(evaluator_path, map_location=DEVICE)
 
 # img_path = "../../face_images/anime-faces/" ##path of anime-faces
 # img_list = pd.read_csv(img_path+'annotations.csv')
-content_path = "../dataset/tsushima_yoshiko/"
-style_path = "../../face_images/img_align_celeba/"
+content_path = "/gpfsnyu/scratch/xg7/vae-transfer-data/dataset/tsushima_yoshiko/"
+style_path = "/gpfsnyu/scratch/xg7/vae-transfer-data/img_align_celeba/"
 
 im_size = 128
 zsize = 512
-style_layers = ["conv%d_bn" % (i) for i in range(2, 7)]
+style_layers = ["conv%d_bn" % (i) for i in range(4, 7)]
 content_layers = ["conv%d_bn" % (i) for i in range(2, 7)]
 style_weight = 0.02
-content_weight = 1.0
+content_weight = 2.0
 
 lr = 1e-4
-n_iters = 4
-log_interval = 2
+n_iters = 5000
+log_interval = 100
 loadSize = 128
 fineSize = 128
-batchSize = 32
+batchSize = 16
 
 encoder = Encoder(zsize, layer_count=5)
 encoder.load_pretrained_dict(pretrained_model)
@@ -60,7 +62,7 @@ content_dataset = Dataset(content_path, loadSize, fineSize)
 content_loader_ = torch.utils.data.DataLoader(dataset     = content_dataset,
                                               batch_size  = batchSize,
                                               shuffle     = True,
-                                              num_workers = 0,
+                                              num_workers = 4,
                                               drop_last   = True)
 content_loader = iter(content_loader_)
 
@@ -68,7 +70,7 @@ style_dataset = Dataset(style_path, loadSize, fineSize)
 style_loader_ = torch.utils.data.DataLoader(dataset     = style_dataset,
                                               batch_size  = batchSize,
                                               shuffle     = True,
-                                              num_workers = 0,
+                                              num_workers = 4,
                                               drop_last   = True)
 style_loader = iter(style_loader_)
 
@@ -94,19 +96,19 @@ for iteration in range(1, n_iters+1):
     except:
         continue
 
-    try:
-        style,_ = style_loader.next()
-    except IOError:
-        style,_ = style_loader.next()
-    except StopIteration:
-        style_loader = iter(style_loader_)
-        style,_ = style_loader.next()
-    except:
-        continue
+    # try:
+    #     style,_ = style_loader.next()
+    # except IOError:
+    #     style,_ = style_loader.next()
+    # except StopIteration:
+    #     style_loader = iter(style_loader_)
+    #     style,_ = style_loader.next()
+    # except:
+    #     continue
 
     animeV.resize_(content.size()).copy_(content)
-    styleV.resize_(style.size()).copy_(style)
-    # styleV, _, _ = vae(animeV)
+    # styleV.resize_(style.size()).copy_(style)
+    styleV, _, _ = vae(animeV)
     sF = encoder(styleV)
     cF = encoder(animeV)
 
@@ -127,10 +129,11 @@ for iteration in range(1, n_iters+1):
     adjust_learning_rate(optimizer, iteration)
 
     if((iteration) % log_interval == 0):
-        transfer = transfer.clamp(0, 1)
-        # style = styleV
+        # transfer = transfer.clamp(0, 1)
+        transfer = transfer * 0.5 + 0.5
+        style = styleV
         concat = torch.cat((content,style,transfer),dim=0)
-        save_image(concat,'%s/%d.png'%('./results_gen/', iteration),normalize=True,scale_each=True,nrow=batchSize)
+        save_image(concat,'%s/%d.png'%('./results_gen', iteration),normalize=True,scale_each=True,nrow=batchSize)
 
     # if(iteration > 0 and (iteration) % opt.save_interval == 0):
-    #     torch.save(matrix.state_dict(), '%s/%s.pth' % ('./results_gen/',opt.layer))
+torch.save(matrix.state_dict(), '%s/%s.pth' % ('/gpfsnyu/scratch/xg7/vae-transfer-data', 'transfer_matrix' ))
